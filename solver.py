33c941e78bd0cb426941c43d1aa1436335c56b1e
gadac <adrickm@gmail.com>
2023-01-19 22:06:51 +0100

    Removed unnecessary comments

--- solver.py
+++ solver.py
@@ -197,24 +197,4 @@ def grid_equilibrium(P_grid, P_core, P_unload):
     else:
         return True
 
-# (reac, hot_tank, cold_tank, storage_load_hx, max_stored_energy, P_unload_max)  = system_initialize(345, 5.5)
-# parameters = system_initialize(345, 5.5)
-
-
-# # P_grid = P_grid_test
-# P_grid = np.array(profil_80EnR_sem_hiver)*(system_max_power/eta)/100
-# (Time, P_core, P_load, P_unload, stored_energy) = load_following(P_grid, *parameters)
-
-# flows = compute_flows(Time, P_core, P_load, P_unload, stored_energy)
-
-# print('')
-# print('Storage capacity: ' + str(int(Joules_to_MWh(max_stored_energy))) + 'MWh')
-# print('Mass of nitrate salt: ' + str(int(hot_tank.V_max*nitrate_salt.rho/1000)) + 't')
-# print('Load factor of reactor: ' + str(load_factor(P_core - P_load)) + '%')
-# print('Load factor of reactor with storage: ' + str(load_factor(P_core)) + '%')
-# print('Consumption-Production equilibrium: ' + str(grid_equilibrium()))
-# print('')
-
-# print_load_graph(Time, P_core, P_load, P_unload, stored_energy, 0, len(P_grid)-1)
-# print_flows(*flows, 0, len(P_grid)-1)
 
